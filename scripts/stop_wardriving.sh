a7cf00889c9896656dcf9714be32e1461f9b6691
btesteracc <bjoern.proepper@gmail.com>
2016-11-02 23:04:13 +0430

    Fixing small issues
    Wait for kismet-server to really stop before stopping other services.
    netxml was not written porperly before. So give kismet-server the time
    to shut down.
    Fixed a type adp instead of adb to stop adb
    Fixed the restart of gpsd in wardriving.sh

--- scripts/stop_wardriving.sh
+++ scripts/stop_wardriving.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-killall kismet_server
-killall gpsd
-killall adp
+/usr/bin/pkill kismet_server
+# warten bis Kismet seine Dateien gespeichert hat
+/usr/bin/pgrep kismet
+while [ $? = 0 ]
+do
+/bin/sleep 1
+/usr/bin/pgrep kismet
+done
+/usr/bin/pkill gpsd
+/bin/sleep 5
+killall adb
 ifconfig wlan1 down
 iwconfig wlan1 mode master
 ifconfig wlan1 up
